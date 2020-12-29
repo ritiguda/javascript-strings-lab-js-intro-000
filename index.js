4268900f0bc812d7d83e3a529892f8de444f1a60
dynamic-chain-5255 <ritiguda@gmail.com>
2020-12-29 22:11:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 var greeting = "Hello, everybody!";
 
-var specialGuest = "Neil deGrasse Tyson"
+var specialGuest = "Neil deGrasse Tyson";
 
-var greetSpecialGuest = "Hello, Everybody!" + "Neil deGrasse Tyson" + "!";
+var greetSpecialGuest = "" + specialGuest ;
 
 var topic = "space";
 
 var conversation = `${topic}`;
+AssertionError: '!' == 'Hello, everybody!'
++ expected - actual
+
+- !
++Hello, everybody!
